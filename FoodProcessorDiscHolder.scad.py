7af477b05f6183f175d8ea36751d0f32ca28e6ab
Thomas Draper <tom@betterservers.com>
2021-04-14 15:19:32 -0700

    Let's get this party started.

---
+++ FoodProcessorDiscHolder.scad.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+'''
+Make a holder for food processor blades
+'''
+
+import solid
+import math
+
+
+outer_d = 155
+outer_w = 2.5
+core_d = 29.5
+core_w = 14
+
+base = solid.sphere([outer_d, 3*core_d, core_d])
+
+final = base
+print(solid.scad_render(final))
